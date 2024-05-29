e24a095e619f559756062d699d75d15ac9f9e8b2
Majisuzz <154841818+Fizzarollii@users.noreply.github.com>
2024-05-29 13:34:39 -0300

    Add files via upload

---
+++ NicolasCanavese/NicolasCanavese/UsuarioData.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicolasCanavese
+{
+    public static class UsuarioData
+    {
+        public class Usuario
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public string NombreUsuario { get; set; }
+            public string Contrasena { get; set; }
+            public string Mail { get; set; }
+
+        }
+
+        public static List<Usuario> ObtenerUsuario(int Id)
+        {
+            List<Usuario> lista = new List<Usuario>();
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion2;Trusted_Connection=True;";
+            var query = "SELECT Id,Nombre,Apellido,NombreUsuario,Constrasena,Mail FROM Usuario WHERE Id=@Id";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    var parametro = new SqlParameter();
+                    parametro.ParameterName = "Id";
+                    parametro.SqlDbType = SqlDbType.Int;
+                    parametro.Value = Id;
+
+                    comando.Parameters.Add(parametro);
+                    using (SqlDataReader dr = comando.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            if (dr.Read())
+                            {
+                                var usuario = new Usuario();
+                                usuario.Id = Convert.ToInt32(dr["ID"]);
+                                usuario.Nombre = dr["Nombres"].ToString();
+                                usuario.Apellido = dr["Apellidos"].ToString();
+                                usuario.NombreUsuario = dr["NombreUsuarios"].ToString();
+                                usuario.Contrasena = dr["Contrasena"].ToString();
+                                usuario.Mail = dr["Mail"].ToString();
+                                lista.Add(usuario);
+                            }
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+        public static List<Usuario> ListarUsuarios()
+        {
+            List<Usuario> lista = new List<Usuario>();
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion2;Trusted_Connection=True;";
+            var query = "SELECT Id,Nombre,Apellido,NombreUsuario,Contrasena,Mail FROM Usuario";
+
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    using (SqlDataReader dr = comando.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            if (dr.Read())
+                            {
+                                var usuario = new Usuario();
+                                usuario.Id = Convert.ToInt32(dr["ID"]);
+                                usuario.Nombre = dr["Nombres"].ToString();
+                                usuario.Apellido = dr["Apellidos"].ToString();
+                                usuario.NombreUsuario = dr["NombreUsuarios"].ToString();
+                                usuario.Contrasena = dr["Contrasena"].ToString();
+                                usuario.Mail = dr["Mail"].ToString();
+                                lista.Add(usuario);
+                            }
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+
+        public static void CrearUsuario(Usuario usuario)
+        {
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion2;Trusted_Connection=True;";
+            var query = "INSERT INTO Usuario (Nombre,Apellido,NombreUsuario,Contrasena,Mail)" +
+                        "VALUES (@Nombre,@Apellido,@NombreUsuario,@Contrasena,@Mail)";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("Nombre", SqlDbType.VarChar) { Value = usuario.Nombre });
+                    comando.Parameters.Add(new SqlParameter("Apellido", SqlDbType.VarChar) { Value = usuario.Apellido });
+                    comando.Parameters.Add(new SqlParameter("NombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario });
+                    comando.Parameters.Add(new SqlParameter("Contrasena", SqlDbType.VarChar) { Value = usuario.Contrasena });
+                    comando.Parameters.Add(new SqlParameter("Mail", SqlDbType.VarChar) { Value = usuario.Mail });
+                }
+                conexion.Close();
+            }
+        }
+
+        public static void ModificarUsuario(Usuario usuario)
+        {
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion2;Trusted_Connection=True;";
+            var query = "UPDATE Usuario" +
+                        "SET Nombre =@Nombre" +
+                        ", Apellido = @Apellido" +
+                        ", NombreUsuario = @NombreUsuario" +
+                        ", Contrasena = @Contrasena" +
+                        ", Mail = @Mail" +
+                        "WHERE Id = @Id";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("Id", SqlDbType.Int) { Value = usuario.Id });
+                    comando.Parameters.Add(new SqlParameter("Nombre", SqlDbType.VarChar) { Value = usuario.Nombre });
+                    comando.Parameters.Add(new SqlParameter("Apellido", SqlDbType.VarChar) { Value = usuario.Apellido });
+                    comando.Parameters.Add(new SqlParameter("NombreUsuario", SqlDbType.VarChar) { Value = usuario.NombreUsuario });
+                    comando.Parameters.Add(new SqlParameter("Contrasena", SqlDbType.Int) { Value = usuario.Contrasena });
+                    comando.Parameters.Add(new SqlParameter("Mail", SqlDbType.VarChar) { Value = usuario.Mail });
+                }
+                conexion.Close();
+            }
+        }
+
+        public static void EliminarUsuario(Usuario usuario)
+        {
+            string connectionString = @"Server=localhost\SQLEXPRESS;Database=SistemaGestion2;Trusted_Connection=True;";
+            var query = "DELETE FROM Usuario WHERE Id = @Id";
+
+            using (SqlConnection conexion = new SqlConnection(connectionString))
+            {
+                conexion.Open();
+                using (SqlCommand comando = new SqlCommand(query, conexion))
+                {
+                    comando.Parameters.Add(new SqlParameter("Id", SqlDbType.Int) { Value = usuario.Id });
+                }
+                conexion.Close();
+            }
+        }
+    }
+}
