cc942a05a5f9bb105ea8578d53455694d687a2da
Majisuzz <154841818+Fizzarollii@users.noreply.github.com>
2024-06-10 20:44:54 -0300

    Add files via upload

---
+++ SistemaGestionBusiness/ProductoVendidoService.cs
@@ -0,0 +1,37 @@
+﻿using SistemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionBussiness
+{
+    internal class ProductoVendidoService
+    {
+        public int Id { get; set; }
+        public static List<PR> ListarProductoVendidos()
+        {
+            return new List<PR>();
+        }
+        public static bool EliminarProductoVendido(int Id)
+        {
+            return true;
+        }
+
+        public static bool CrearProductoVendido(PR productoVendido)
+        {
+            return true;
+        }
+
+        public static bool ModificarProductoVendido(int Id, PR productoVendido)
+        {
+            return true;
+        }
+
+        public static PR ObtenerProductoVendido(int Id)
+        {
+            return new PR();
+        }
+    }
+}
---
+++ SistemaGestionBusiness/UsuarioService.cs
@@ -0,0 +1,38 @@
+﻿using NicolasCanavese;
+using SistemaGestionEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionBussiness
+{
+    public class UsuarioService
+    {
+        public int Id { get; set; }
+        public static List<Usuario> ListarUsuarios()
+        {
+            return new List<Usuario>();
+        }
+        public static bool EliminarUsuario(int Id)
+        {
+            return true;
+        }
+
+        public static bool CrearUsuario(Usuario usuario)
+        {
+            return true;
+        }
+
+        public static bool ModificarUsuario(int Id, Usuario usuario)
+        {
+            return true;
+        }
+
+        public static Usuario ObtenerUsuario(int Id)
+        {
+            return new Usuario();
+        }
+    }
+}
