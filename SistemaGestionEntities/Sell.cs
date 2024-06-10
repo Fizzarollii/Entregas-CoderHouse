b1fc25251cfef8a946a615933143927232767bb1
Majisuzz <154841818+Fizzarollii@users.noreply.github.com>
2024-06-10 20:46:25 -0300

    Add files via upload

---
+++ SistemaGestionEntities/Sell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaGestionEntities
+{
+    public class Sell
+    {
+        public class Venta
+        {
+            public int Id { get; set; }
+            public string? Comentarios { get; set; }
+            public int IdUsuario { get; set; }
+        }
+    }
+}
